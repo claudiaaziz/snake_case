9a5aeb33f69f008d5a4939c642b7fe253b7f5917
claudiaaziz <claudiahannahh@gmail.com>
2024-05-18 20:21:08 -0400

    Add resizable and draggable image feature using Rnd

--- src/app/configure/design/page.tsx
+++ src/app/configure/design/page.tsx
@@ -1,3 +1,7 @@
+import { db } from '@/db';
+import { notFound } from 'next/navigation';
+import DesignConfigurator from './DesignConfigurator';
+
 interface PageProps {
   searchParams: {
     [key: string]: string | string[] | undefined;
@@ -6,9 +10,25 @@ interface PageProps {
 
 const Page = async ({ searchParams }: PageProps) => {
   const { id } = searchParams;
+
+  if (!id || typeof id !== 'string') return notFound();
+
+  const configuration = await db.configuration.findUnique({
+    where: { id },
+  });
+
+  if (!configuration) return notFound();
+
+  const { imageUrl, height, width } = configuration;
   // make db call
 
-  return <div>{id}</div>;
+  return (
+    <DesignConfigurator
+      configId={configuration.id}
+      imageUrl={imageUrl}
+      imageDimensions={{ height, width }}
+    />
+  );
 };
 
 export default Page;
